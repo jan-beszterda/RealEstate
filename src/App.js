d58b6502331b1b8ca0c12122b0f26c04900a2a2e
jan-beszterda <jan.beszterda@gmail.com>
2022-02-19 16:46:42 +0100

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Footer />
-      </div>
-      <div className="container-fluid">
-        <Routes>{/*<Route path="/" element={<Home />} />*/}</Routes>
-      </div>
-    </BrowserRouter>
-  );
+  return <div></div>;
 }
 
 export default App;
