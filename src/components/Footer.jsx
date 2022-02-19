3640f6ca0665acce5e8274ca1aed014728a91c40
jan-beszterda <jan.beszterda@gmail.com>
2022-02-19 16:19:25 +0100

    Updated footer with better style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,114 +2,48 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer
-      className="container-fluid py-3 my-4"
-      style={{
-        backgroundColor: "#D3D3D3",
-        maxHeight: "100px",
-        fontSize: "0.8rem",
-        lineHeight: "1rem",
-        fontWeight: "bold",
-      }}
-    >
+    <footer className="container-fluid py-3 my-4">
       <div className="row">
         <ul className="nav justify-content-center align-items-center pb-2 mb-2">
           <li className="nav-item">
-            <Link
-              to="/"
-              className="nav-link px-2"
-              style={{
-                color: "#000000",
-                textTransform: "uppercase",
-                letterSpacing: "1px",
-              }}
-            >
+            <Link to="/" className="nav-link px-2">
               Hem
             </Link>
           </li>
-          <span style={{ color: "#000000" }}>&middot;</span>
+          <span>&middot;</span>
           <li className="nav-item">
-            <Link
-              to="/"
-              className="nav-link px-2"
-              style={{
-                color: "#000000",
-                textTransform: "uppercase",
-                letterSpacing: "1px",
-              }}
-            >
+            <Link to="/" className="nav-link px-2">
               Tjänster
             </Link>
           </li>
-          <span style={{ color: "#000000" }}>&middot;</span>
+          <span>&middot;</span>
           <li className="nav-item">
-            <Link
-              to="/"
-              className="nav-link px-2"
-              style={{
-                color: "#000000",
-                textTransform: "uppercase",
-                letterSpacing: "1px",
-              }}
-            >
+            <Link to="/" className="nav-link px-2">
               Objekt till salu
             </Link>
           </li>
-          <span style={{ color: "#000000" }}>&middot;</span>
+          <span>&middot;</span>
           <li className="nav-item">
-            <Link
-              to="/"
-              className="nav-link px-2"
-              style={{
-                color: "#000000",
-                textTransform: "uppercase",
-                letterSpacing: "1px",
-              }}
-            >
+            <Link to="/" className="nav-link px-2">
               Om oss
             </Link>
           </li>
-          <span style={{ color: "#000000" }}>&middot;</span>
+          <span>&middot;</span>
           <li className="nav-item">
-            <Link
-              to="/"
-              className="nav-link px-2"
-              style={{
-                color: "#000000",
-                textTransform: "uppercase",
-                letterSpacing: "1px",
-              }}
-            >
+            <Link to="/" className="nav-link px-2">
               Blogg
             </Link>
           </li>
-          <span style={{ color: "#000000" }}>&middot;</span>
+          <span>&middot;</span>
           <li className="nav-item">
-            <Link
-              to="/"
-              className="nav-link px-2"
-              style={{
-                color: "#000000",
-                textTransform: "uppercase",
-                letterSpacing: "1px",
-              }}
-            >
+            <Link to="/" className="nav-link px-2">
               Kontakta oss
             </Link>
           </li>
         </ul>
       </div>
       <div className="row">
-        <p
-          className="text-center"
-          style={{
-            color: "#000000",
-            textTransform: "uppercase",
-            letterSpacing: "1px",
-          }}
-        >
-          &copy; 2022 Real Estate AB
-        </p>
+        <p className="text-center">&copy; 2022 Real Estate AB</p>
       </div>
     </footer>
   );
