f5f2bf2f9332b483e5ec1e99d5d16d3c079ab320
jan-beszterda <jan.beszterda@gmail.com>
2022-02-21 11:34:58 +0100

    branch code cleanup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import ContactUs from "./components/ContactUs";
 
 function App() {
-  return (
-    <main className="container-sm my-3">
-      <ContactUs />
-    </main>
-  );
+  return <main className="container-sm my-3"></main>;
 }
 
 export default App;
