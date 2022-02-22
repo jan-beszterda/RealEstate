9135c0386cc2651419d78a82c8cea0487cdf4096
Liban <libanmoh9898@gmail.com>
2022-02-23 00:20:11 +0100

    LoginForm

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,11 +10,14 @@ const [value, setValue] = useState({
   password: '',
 })
 
+const [submitet, setSubmitet] = useState(false)
+
 // min metod för det som sker när man har matat in sina uppgifter
-  function submitHandler(){
-  Event.preventDefault()
-    console.log("submitet")
-  }
+
+ const handleSubmit = (event) => {
+   event.preventDefault();
+   setSubmitet(true);
+ }
 // (event) är det som händer  inom input rutorna jag ber programmet att kopiera in den tomma strängen (...) för att sedan setta värdet till det som skrivs in i input rutan detta kallas inom onChange
 
   const userNameHandler  = (event) =>{
@@ -23,23 +26,33 @@ const [value, setValue] = useState({
   const passwordHandler  = (event) =>{
     setValue({...value, password: event.target.value})
   }
+
+ 
+
   return (
+    <div > 
+    {handleSubmit && !value.userName ? <span className="username-enter">Enter your username please.</span> : null}
+    {handleSubmit && !value.password ? <span className="password-enter">Enter your password please.</span> : null}
      <div className="login-container">
-     <form><div>
+     {submitet ? <div className="succses-msg">Submitet</div> : null}
+     <form onSubmit = {handleSubmit}>
+     <div>
      <input type="text" className="username-input" value = {value.userName} onChange={userNameHandler}></input>
      <p className="login-username">Username:</p>
+    
      </div>
      
-    <div>
+    <div className="password-container">
      <input type="text" className="password-input" value = {value.password} onChange={passwordHandler}></input>
      <p className="login-password">Password:</p>
+     
      </div>
 
-     <button className="login-btn" type="submit" onClick={submitHandler}>Log In</button>
+     <button className="login-btn" type="submit" >Log In</button>
      </form>
      
      </div>
-   
+   </div>
 
    
   )
