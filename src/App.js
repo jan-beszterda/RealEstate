fb3af4e3615bde848300f51b136dbebd13ddb157
Liban <libanmoh9898@gmail.com>
2022-02-22 14:13:48 +0100

    LoginBtn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Login from "./components/Login"
 
 function App() {
-  return <main className="container-sm my-3"></main>;
+  return <main className="container-sm my-3">
+  <Login/></main>;
 }
 
 export default App;
