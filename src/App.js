6d6ef3cc497947496e46e12129c9a35e27e8875f
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-02-22 11:56:25 +0100

    componenet formulär för bokning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ContactForm from "./components/ContactForm";
+import BookingForm from "./components/BookingForm";
 
 function App() {
   return (
     <div className="container">
-      <ContactForm />
+      <BookingForm />
     </div>
   );
 }
