9761f8a5b1ce93690cdd7a423bb36566a02b0303
jan-beszterda <jan.beszterda@gmail.com>
2022-02-23 11:20:59 +0100

    Moved state to the ContactUs component

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -7,10 +7,62 @@ import {
   faPhone,
   faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const ContactUs = () => {
+  const [contact, setContact] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+    message: "",
+  });
+
+  const [contacts, setContacts] = useState([]);
+
+  const [success, setSuccess] = useState(false);
+
+  const handleFieldChange = (fieldName, fieldValue) => {
+    setContact({ ...contact, [fieldName]: fieldValue });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      contact.firstName &&
+      contact.lastName &&
+      contact.phone &&
+      contact.email &&
+      contact.message
+    ) {
+      const newContact = { ...contact, id: uuidv4() };
+      setContacts([...contacts, newContact]);
+      setContact({
+        firstName: "",
+        lastName: "",
+        phone: "",
+        email: "",
+        message: "",
+      });
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("Contacts", JSON.stringify([...contacts]));
+  }, [contacts]);
+
   return (
     <>
+      {success && (
+        <div className="overlay">
+          <span className="text-center">Thank you for your message</span>
+        </div>
+      )}
       <div className="row mb-4">
         <h2 className="text-center">Kontakta oss</h2>
       </div>
@@ -46,7 +98,11 @@ const ContactUs = () => {
         </Section>
         <div className="w-100 d-block d-md-none"></div>
         <div className="col d-flex flex-column justify-content-center">
-          <ContactForm />
+          <ContactForm
+            data={contact}
+            onChange={handleFieldChange}
+            submit={handleSubmit}
+          />
         </div>
       </div>
     </>
