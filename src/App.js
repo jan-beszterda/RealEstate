c820768eadf35345f37db9039219905896fd366c
jan-beszterda <jan.beszterda@gmail.com>
2022-02-23 21:04:23 +0100

    fixed typo error in index.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from "react";
-import BookingForm from "./components/BookingForm";
-import Services from "./components/Services";
-import Blogg from "./components/Blogg";
 
 function App() {
   return <main className="container-sm my-3"></main>;
