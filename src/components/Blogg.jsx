97cb7756625d6d6c1ec74f90d3f6b5db51319d05
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2022-02-25 15:16:11 +0100

    Blogg component done

--- src/components/Blogg.jsx
+++ src/components/Blogg.jsx
@@ -2,30 +2,35 @@ import React from 'react'
 import Section from './Section'
 import Button from './Button'
 
+
 function Blogg() {
   return (
     <div>
         <div>
             <h1>Läs nyheter från bostadsmarkanden</h1>
         </div>
-        <Section>
-            <img src="" alt="" />
+        <Section className={"row"}>
+            <div className='col-3'><img className='blogg-pic' src="/bild/house.jpg" alt="" /></div>
+            <div className='col-9'>
             <h3>Artikel headline</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem blandit, elementum orci ut, ullamcorper lacus.</p>
-            <Button className="btn btn-primary mb-4">Läs mer</Button>
+            <Button className="btn btn-primary mb-4">Läs mer</Button></div>
         </Section>
-        <Section>
-            <img src="" alt="" />
+        <Section className={"row"}>
+            <div className='col-3'><img className='blogg-pic' src="/bild/house.jpg" alt="" /></div>
+            <div className='col-9'>
             <h3>Artikel headline</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem blandit, elementum orci ut, ullamcorper lacus.</p>
-            <Button className="btn btn-primary mb-4">Läs mer</Button>
+            <Button className="btn btn-primary mb-4">Läs mer</Button></div>
         </Section>
-        <Section>
-            <img src="" alt="" />
+        <Section className={"row"}>
+            <div className='col-3'><img className='blogg-pic' src="/bild/house.jpg" alt="" /></div>
+            <div className='col-9'>
             <h3>Artikel headline</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem blandit, elementum orci ut, ullamcorper lacus.</p>
-            <Button className="btn btn-primary mb-4">Läs mer</Button>
+            <Button className="btn btn-primary mb-4">Läs mer</Button></div>
         </Section>
+        
     </div>
   )
 }
