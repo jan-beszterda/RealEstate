f4741435f88cb40ab89da85e38559de28afab228
HaNNa89 <petersson.h@hotmail.com>
2022-02-28 15:20:13 +0100

    created header, about page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,38 @@
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div>
-      <img src="" alt="" /> 
+    <header className="flex-container">
+      <div class="text-center">
+       <Link className="nav-link" to="/">
+         <div className="header-image">
+           <img className="header-image" src="images/logga-notagline.png" alt="logo"/>
+         </div>
+        </Link>
       </div>
-      <nav>
-        <ul>
-        <li><Link className="" to="/">
-            Hem</Link></li>
-        <li><Link className="" to="/Services">
-            Tjänster</Link></li>
-        <li><Link className="" to="/ObjectForSale">
-            Objekt till salu</Link></li>
-        <li><Link className="" to="/About">
-            Om oss</Link></li>
-        <li><Link className="" to="/Blog">
-            Blogg</Link></li>
-        <li><Link className="" to="/ContactUs">
-            Kontakta oss</Link></li>
-        </ul>
-      </nav>
+        <nav className="navbar navbar-expand-sm justify-content-center">     
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+              Hem</Link></li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/services">
+              Tjänster</Link></li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/forsale">
+              Objekt till salu</Link></li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+              Om oss</Link></li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/blog">
+              Blogg</Link></li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+              Kontakta oss</Link></li>
+          </ul>
+        </nav>
     </header>
   );
 };
