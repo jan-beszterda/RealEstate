b6d9ab1f78a543420d7d42e44d4c7a36c0b984d7
jan-beszterda <jan.beszterda@gmail.com>
2022-03-02 12:51:08 +0100

    updated homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,57 +1,50 @@
-import React, {useState, useEffect} from "react"; 
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import Section from "./Section"; 
+import Section from "./Section";
+import Button from "./Button";
 
-import Button from "./Button"; 
+const Homepage = () => {
+  useEffect(
+    () => document.getElementById("main").classList.add("main-image"),
+    []
+  );
 
- 
- 
+  useEffect(
+    () => () => document.getElementById("main").classList.remove("main-image"),
+    []
+  );
 
-const Homepage = () => { 
+  return (
+    <div className="container-sm" style={{ height: "100vh" }}>
+      <Section className="section-home section-grey p-4">
+        <h1 className="my-3">Välkommen till Real Estate</h1>
+        <p style={{ fontSize: "1.2rem" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.{" "}
+        </p>
+        <p style={{ fontSize: "1.2rem" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.{" "}
+        </p>
+        <p style={{ fontSize: "1.2rem" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.{" "}
+        </p>
+        <Link to="/">
+          <Button className={"btn btn-primary btn-lg"} type="button">
+            Klicka här
+          </Button>
+        </Link>
+      </Section>
+    </div>
+  );
+};
 
-    useEffect (() => { 
-
-        document.getElementById("main").classList.toggle("main-image");  
-
-        } ) 
-
-    return ( 
-
-         
-
-        <div className="container"> 
-
-         
-
-        <Section className="section-home"> 
-
-        <h1>Real Estate</h1> 
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,  
-
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,  
-
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p> 
-
-        <Button>Klicka här</Button> 
-
-        </Section> 
-
-        
-
-        </div> 
-
- 
- 
-
-    ); 
-
-}; 
-
- 
- 
-
-export default Homepage; 
-
+export default Homepage;
