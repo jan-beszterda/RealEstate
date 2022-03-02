bcca7e6312dd97180679c1770fcaade010eb421e
jan-beszterda <jan.beszterda@gmail.com>
2022-03-02 09:24:14 +0100

    Updated blog css, made it responsive

--- src/components/Blogg.jsx
+++ src/components/Blogg.jsx
@@ -1,76 +1,95 @@
-import React, {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-import Section from './Section'
-import Button from './Button'
-import Login from './Login'
-
+import Section from "./Section";
+import Button from "./Button";
+import Login from "./Login";
 
 function Blogg() {
+  const [login, setLogin] = useState({
+    userName: "",
+    password: "",
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const [login, setLogin] = useState({ //useState for login data (username and password)
+  const handleFieldChange = (fieldName, fieldValue) => {
+    setLogin({ ...login, [fieldName]: fieldValue });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (login.userName === "admin" && login.password === "password") {
+      setLogin({
         userName: "",
         password: "",
       });
-    const [isLoggedIn, setIsLoggedIn] = useState(false); //useState for successfull login
+      setIsLoggedIn(true);
+    }
+  };
 
-    const handleFieldChange = (fieldName, fieldValue) => { //Login data is set to what comes from the form
-        setLogin({ ...login, [fieldName]: fieldValue });
-      };
-
-    const handleSubmit = (e) => { //When form is submitted and username and password are correct login is set to true
-        e.preventDefault();
-        if (
-          login.userName === "admin" &&
-          login.password === "password"
-        ) {
-          setLogin({
-            userName: "",
-            password: "",
-          });
-          setIsLoggedIn(true);
-        }
-      };
-    
-      useEffect(() => { //Log in (true/false) is saved in local storage 
-        localStorage.setItem("Logged in", JSON.stringify([isLoggedIn]));
-      }, [isLoggedIn]);
-    
-     //Login component med props (login, change and submit methods)
-
-    
+  useEffect(() => {
+    localStorage.setItem("Logged in", JSON.stringify([isLoggedIn]));
+  }, [isLoggedIn]);
 
   return (
-    <div>
-        <div>
-            <h1>Läs nyheter från bostadsmarkanden</h1>
+    <div className="container-sm">
+      <Login
+        loginData={login}
+        onChange={handleFieldChange}
+        submit={handleSubmit}
+      />
+      <div className="row mb-4">
+        <h1>Läs nyheter från bostadsmarkanden</h1>
+      </div>
+      <Section className={"section-grey row row-cols-1 row-cols-md-2 mb-4 p-3"}>
+        <div className="col-md-4">
+          <img className="blogg-pic img-fluid" src="/bild/house.jpg" alt="" />
         </div>
-        <div className='d-flex justify-content-end'>
-            <Login loginData={login} onChange={handleFieldChange} submit={handleSubmit}></Login>
+        <div className="col-md-8 mt-3 mt-md-0">
+          <h3>Artikel headline</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed
+            tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut
+            iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem
+            blandit, elementum orci ut, ullamcorper lacus.
+          </p>
+          <Button className="btn btn-primary btn-lg">Läs mer</Button>
         </div>
-        <Section className={"row"}>
-            <div className='col-3 h-100'><img className='blogg-pic' src="/bild/house.jpg" alt="" /></div>
-            <div className='col-9 h-100'>
-            <h3>Artikel headline</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem blandit, elementum orci ut, ullamcorper lacus.</p>
-            <Button className="btn btn-primary mb-4">Läs mer</Button></div>
-        </Section>
-        <Section className={"row"}>
-            <div className='col-3 h-100'><img className='blogg-pic' src="/bild/house.jpg" alt="" /></div>
-            <div className='col-9 h-100'>
-            <h3>Artikel headline</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem blandit, elementum orci ut, ullamcorper lacus.</p>
-            <Button className="btn btn-primary mb-4">Läs mer</Button></div>
-        </Section>
-        <Section className={"row"}>
-            <div className='col-3 h-100'><img className='blogg-pic' src="/bild/house.jpg" alt="" /></div>
-            <div className='col-9 h-100'>
-            <h3>Artikel headline</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem blandit, elementum orci ut, ullamcorper lacus.</p>
-            <Button className="btn btn-primary mb-4">Läs mer</Button></div>
-        </Section>
-        
+      </Section>
+      <Section className={"section-grey row row-cols-1 row-cols-md-2 mb-4 p-3"}>
+        <div className="col-md-4">
+          <img className="blogg-pic img-fluid" src="/bild/house.jpg" alt="" />
+        </div>
+        <div className="col-md-8 mt-3 mt-md-0">
+          <h3>Artikel headline</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed
+            tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut
+            iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem
+            blandit, elementum orci ut, ullamcorper lacus.
+          </p>
+          <Button className="btn btn-primary btn-lg">Läs mer</Button>
+        </div>
+      </Section>
+      <Section className={"section-grey row row-cols-1 row-cols-md-2 mb-4 p-3"}>
+        <div className="col-md-4">
+          <img className="blogg-pic img-fluid" src="/bild/house.jpg" alt="" />
+        </div>
+        <div className="col-md-8 mt-3 mt-md-0">
+          <h3>Artikel headline</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            suscipit pretium arcu ut sollicitudin. Proin non lorem est. Sed
+            tempus nulla nulla. Sed tempor velit ut facilisis ullamcorper. Ut
+            iaculis velit vitae enim rhoncus luctus id rutrum nisl. Sed sed sem
+            blandit, elementum orci ut, ullamcorper lacus.
+          </p>
+          <Button className="btn btn-primary btn-lg">Läs mer</Button>
+        </div>
+      </Section>
     </div>
-  )
+  );
 }
 
-export default Blogg
+export default Blogg;
