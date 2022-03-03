016057b93a56c6f0da0cb11a4832ce9219f13fb9
jan-beszterda <jan.beszterda@gmail.com>
2022-03-03 16:41:27 +0100

    updated homepage and about us page

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,12 @@ const About = () => {
       <Section className={"d-flex flex-column"}>
         <h1 className="text-center mb-3">Om Real Estate</h1>
         <img
-          className="img-fluid align-self-center mb-3"
-          style={{ maxWidth: "50rem" }}
-          src="/images/living-room4.jpg" alt="..."
+          className="img-fluid about-image align-self-center mb-3"
+          src="/images/living-room4.jpg"
+          alt="..."
         />
         <p style={{ fontSize: "1.2rem" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
           ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
           tempor incididunt ut labore et dolore magna aliqua.
